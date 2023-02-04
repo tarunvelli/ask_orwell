9a5cf335662b9d38c8d1a47a0772b5c239180dee
Tarun Vellishetty <tarunvelli@gmail.com>
2023-02-04 20:01:52 +0530

    questions index page

---
+++ app/javascript/components/Questions.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Questions = () => {
+  const navigate = useNavigate();
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const url = "/api/v1/questions";
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((res) => setQuestions(res))
+      .catch(() => navigate("/"));
+  }, []);
+
+  const allQuestions = questions.map((question, index) => (
+    <div key={index} className="col-md-6 col-lg-4">
+      <div className="card mb-4">
+        <div className="card-body">
+          <h5 className="card-title">{question.prompt}</h5>
+          <Link to={`/questions/${question.id}`} className="btn btn-dark">
+            View Question
+          </Link>
+        </div>
+      </div>
+    </div>
+  ));
+  const noQuestion = (
+    <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+      <h4>
+        No questions yet. Why not <Link to="/new_question">create one</Link>
+      </h4>
+    </div>
+  );
+
+  return (
+    <>
+      <section className="jumbotron jumbotron-fluid text-center">
+        <div className="container py-5">
+          <h1 className="display-4">Questions</h1>
+          <p className="lead text-muted">
+            We’ve pulled together our most popular questions
+          </p>
+        </div>
+      </section>
+      <div className="py-5">
+        <main className="container">
+          <div className="text-end mb-3">
+            <Link to="/question" className="btn custom-button">
+              Create New Question
+            </Link>
+          </div>
+          <div className="row">
+            {questions.length > 0 ? allQuestions : noQuestion}
+          </div>
+          <Link to="/" className="btn btn-link">
+            Home
+          </Link>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Questions;
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "../components/Welcome";
+import Questions from "../components/Questions";
 
 export default (
   <Router>
     <Routes>
-      <Route path="/" element={<Welcome />} />
+      <Route path="/" exact element={<Welcome />} />
+      <Route path="/questions" element={<Questions />} />
     </Routes>
   </Router>
 );
